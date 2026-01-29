34271bfe76c3b19ac06dc914c15959356850dade
Nqv4 <215499124+ProblemEdge@users.noreply.github.com>
2026-01-29 20:13:37 +0900

    changed

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,17 @@
 import { PrismaClient } from '@prisma/client';
 
-// グローバルスコープにPrismaClientを保存して、再利用する
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+// グローバルスコープにPrismaClientを保存して、再利用する（開発環境のみ）
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['info', 'warn', 'error'] : ['warn', 'error'],
+    log:
+      process.env.NODE_ENV === 'development'
+        ? ['query', 'info', 'warn', 'error']
+        : ['warn', 'error'],
   });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV === 'development' && !globalForPrisma.prisma) {
+  globalForPrisma.prisma = prisma;
+}
