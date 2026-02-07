8d899ab92dea4adf6c34cbe8567e0606dd2058fe
Nqv4 <215499124+ProblemEdge@users.noreply.github.com>
2026-02-07 10:30:39 +0900

    余白を追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -68,7 +68,7 @@ export default function RootLayout({
           #root-wrapper {
             height: 100vh;
             padding-top: var(--safe-area-top);
-            padding-bottom: var(--safe-area-bottom);
+            padding-bottom: calc(var(--safe-area-bottom, 0px) + 12rem);
             padding-left: var(--safe-area-left);
             padding-right: var(--safe-area-right);
             box-sizing: border-box;
